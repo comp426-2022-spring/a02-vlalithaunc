f84f9eaba6ed09819b65c732fb682f111e93f980
vlalithaunc <vlalitha@email.unc.edu>
2022-03-20 12:48:31 -0400

    changed guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -7,10 +7,15 @@ const require = createRequire(import.meta.url);
 
 const args = require('minimist')(process.argv.slice(2));
 
-const err = new Error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
+const err_null = new Error("Error: no input.\nUsage: node guess-flip --call=[heads|tails]");
 
-if(args.call == null || (args.call != "heads" && args.call != "tails")){
-    console.error(err.message);
+const err_incorrect = new Error("Error: wrong input.\nUsage: node guess-flip --call=[heads|tails]");
+
+if(args.call == null){
+    console.error(err_null.message);
+}
+else if(args.call != "heads" && args.call != "tails"){
+    console.error(err_incorrect.message);
 }
 else{
     console.log(flipACoin(args.call));
